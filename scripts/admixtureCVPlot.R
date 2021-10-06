f39fbdb6e19a68bf3ac44f3adf347cb17334123a
jdselwyn <jdselwyn@gmail.com>
2021-10-06 10:18:56 -0500

    update

--- scripts/admixtureCVPlot.R
+++ scripts/admixtureCVPlot.R
@@ -6,12 +6,14 @@ outNAME <- args[1]
 
 suppressWarnings(suppressMessages(library(tidyverse)))
 
-cv_plot <- read_lines(str_c(outNAME, '.CV_error')) %>%
+cv_data <- read_lines(str_c(outNAME, '.CV_error')) %>%
   tibble(line = .) %>%
   mutate(K = str_extract(line, '(K=)[0-9]+') %>%
            str_remove('K=') %>% as.integer,
          Error = str_extract(line, '[0-9\\.]+$') %>%
-           as.numeric) %>%
+           as.numeric)
+
+cv_plot <- cv_data %>%
   ggplot(aes(x = K, y = Error)) +
   geom_line() +
   geom_point(aes(colour = Error == min(Error))) +
@@ -22,3 +24,4 @@ cv_plot <- read_lines(str_c(outNAME, '.CV_error')) %>%
   theme(legend.position = 'none')
 ggsave(str_c(outNAME, '.cv_plot.png'), plot = cv_plot, width = 7, height = 7)
 
+write(cv_data$K[cv_data$Error == min(cv_data$Error)][1], stdout())
