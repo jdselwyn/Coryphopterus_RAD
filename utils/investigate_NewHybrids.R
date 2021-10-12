17827f1330274a3b6ec5085b932b6e737128c7e7
jdselwyn <jdselwyn@gmail.com>
2021-10-12 17:16:08 -0500

    Update investigate_NewHybrids.R

--- utils/investigate_NewHybrids.R
+++ utils/investigate_NewHybrids.R
@@ -1,5 +1,7 @@
 library(tidyverse)
 
 mtdna <- read_csv('../Mitochondrial_Mapping/blast_speciesID.csv') 
-admix_data <- read_csv()
-newhybrids <- read_csv()
+admix_data <- read_csv('../splitSpecies/ADMIXTURE/MiSeq_lightSpecies2.10.1.2.results.csv')
+newhybrids <- read_csv('../splitSpecies/newHybrids/newHybrids_fullResults.csv')
+
+
