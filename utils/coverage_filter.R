3502a45d60619376983cd4224bb1e708d5902e98
jdselwyn <jdselwyn@gmail.com>
2021-10-30 10:32:42 -0500

    updates

--- utils/coverage_filter.R
+++ utils/coverage_filter.R
@@ -15,7 +15,7 @@ library(tidyverse)
 library(magrittr)
 library(fitdistrplus)
 
-mean_depth <- read_lines('../tmp_dir/MiSeq_CHYA_chyaH.2.1.Fltr04.18.site.depth.mean') %>%
+mean_depth <- read_lines('../tmp_dir/MiSeq_CHYA_chyaI.2.1.Fltr04.6.site.depth.mean') %>%
   extract(-1) %>%
   as.numeric()
 
@@ -64,52 +64,75 @@ out <- read_delim('../tmp_dir/meandepthVSvariance', delim = '\t',
   dplyr::rename(m = mean, s = sd) %>%
   mutate(id = str_c('R', row_number()),
          l_m = log(m) - 0.5 * log((s/m)^2 + 1),
-         l_s = sqrt(log((s/m)^2 + 1))) %>%
+         l_s = sqrt(log((s/m)^2 + 1)),
+         tries = 100) %>%
   rowwise %>%
   partition(cluster) %>%
-  mutate(n_less = sum(rlnorm(5000, l_m, l_s) <= 3),
-         tidy(binom.test(x = n_less, n = 5000))) %>%
+  mutate(n_less = sum(rlnorm(tries, l_m, l_s) <= 3),
+         tidy(binom.test(x = n_less, n = tries))) %>%
   collect
 
 out %>%
   filter(m > 100) %>%
-  ggplot(aes(x = m, y = n_less / 5000)) +
+  sample_n(500) %>%
+  ggplot(aes(x = m, y = n_less / tries)) +
   geom_point()
 
 library(emmeans)
 library(brms)
 
+filter(out, !(n_less == 0 | n_less == tries))
+
+
+
+tst <- glm(cbind(n_less, tries - n_less) ~ m * s, 
+           data = mutate(out, across(c(n_less, tries), ~. + 1)),
+           family = 'binomial') 
+summary(tst)
+
+
+get_prior(n_less | trials(tries) ~ m * s,
+          family = 'binomial',
+          data = out)
+
+inits <- list(
+  Intercept = -4.5e-1,
+  beta     = c(-6e-2, 1e-2, 2e-5)
+)
+
+tst <- brm(n_less | trials(tries) ~ m * s,
+           family = 'binomial',
+           prior = prior(normal(-4.5e-1, 1e-3), class = 'Intercept') +
+             prior(normal(-6e-2, 3e-5), class = 'b', coef = 'm') +
+             prior(normal(1e-2, 1.5e-5), class = 'b', coef = 's') +
+             prior(normal(2e-5, 9e-8), class = 'b', coef = 'm:s'),
+           data = filter(out, !(n_less == 0 | n_less == tries)),
+           
+           inits = list(inits, inits),
+           
+           backend = 'cmdstanr',
+           chains = 2,
+           cores = 2,
+           iter = 500, 
+           warmup = 500)
+
 library(lme4)
+tst <- glmer(cbind(n_less, tries - n_less) ~ m * s + (1 | id), 
+             data = mutate(out, across(c(n_less, tries), ~. + 1)),
+             family = 'binomial')
 
-tst <- brm(n_less | trials(5000) ~ m * s + (1 | id),
-           family = 'binomial',
-           data = out)
-
-tst <- glmer(cbind(n_less, 5000 - n_less) ~ m * s + (1 | id),
-           family = 'binomial',
-           data = out)
-
-
-glm(cbind(n_less, 5000 - n_less) ~ m * s, data = out,
-    family = 'binomial') %>%
+tst %>%
   emmeans(., ~ m, by = 's', type = 'response',
-          at=list(m = seq(1, max(out$m), length.out = 100))) %>%
+          at=list(m = seq(1, max(out$m), length.out = 1000))) %>%
   tidy(conf.int = TRUE) %>%
+  mutate(across(c(prob, asymp.LCL, asymp.UCL), ~.*593)) %>%
   ggplot(aes(x = m, y = prob, ymin = asymp.LCL, ymax = asymp.UCL)) +
+  geom_point(data = sample_n(out, 1000), aes(x = m, y = n_less/tries * 593),
+             inherit.aes = FALSE) +
   geom_ribbon() +
-  geom_line() +
+  geom_line(colour = 'red') +
+  scale_x_continuous(limits = c(NA, NA)) +
   theme_classic()
   
-
-
-out %>%
-  arrange(-m) %>%
-  dplyr::select(-n_less, -statistic:-parameter, -method:-alternative) %>%
-  mutate(across(c(estimate, starts_with('conf')), ~ .*593)) %>%
-  
-
-  ggplot(aes(x = m*s, y = estimate)) + 
-  geom_point()
-
 
 
